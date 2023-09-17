5c43e297669b079b50210821d705be92e36b7435
IbrahimHindawi <ihindawi@gmail.com>
2023-09-17 08:08:09 +0400

    added VS devenv script

--- scripts/init.bat
+++ scripts/init.bat
@@ -3,3 +3,13 @@ echo System Initalized
 start C:\Scripts\automation\transp.ahk
 start C:\Scripts\automation\winterm.ahk
 start C:\Scripts\automation\uncap.exe
+
+pushd "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools"
+cmd /c "VsDevCmd.bat&set" |
+foreach {
+  if ($_ -match "=") {
+    $v = $_.split("=", 2); set-item -force -path "ENV:\$($v[0])"  -value "$($v[1])" 
+  }
+}
+popd
+Write-Host "`nVisual Studio 2022 Command Prompt variables set." -ForegroundColor Yellow
