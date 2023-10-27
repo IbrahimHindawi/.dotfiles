cb1612a3228a926b01516f0094114a4fd39e8b2b
Ibrahim Hindawi <ihindawi@gmail.com>
2023-10-27 18:47:14 +0400

    added linux shell automation

---
+++ init.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mkdir ~/.config/nvim
+cp nvim/ginit.vim ~/.config/nvim
+cp nvim/init.lua ~/.config/nvim
+
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
