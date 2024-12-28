c0d2ddffcef98e9efb498bd88beb50ebd6302c13
IbrahimHindawi <ihindawi@gmail.com>
2024-12-28 16:11:19 +0400

    updated luasnip

--- nvim/lua/plugins/luasnip.lua
+++ nvim/lua/plugins/luasnip.lua
@@ -11,7 +11,7 @@ return {
     imap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
     smap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
     ]]
-    require("luasnip.loaders.from_lua").load({paths = "/Users/Administrator/AppData/Local/nvim/LuaSnip/"})
+    require("luasnip.loaders.from_lua").load({paths = "%USERPROFILE%/AppData/Local/nvim/LuaSnip/"})
     -- require("luasnip.loaders.from_lua").load({paths = "%USERPROFILE%\\AppData\\Local\\nvim\\LuaSnip\\"})
     -- require("luasnip.loaders.from_lua").load({paths = "~/.config/nvim/LuaSnip/"})
     -- Set up lspconfig.
