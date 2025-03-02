956807210490519fa5faeb310ca54ee76fb4fb22
IbrahimHindawi <ihindawi@gmail.com>
2025-03-02 23:27:11 +0400

    updated init.bat

--- init.bat
+++ init.bat
@@ -1,8 +1,8 @@
 @echo off
 mkdir %USERPROFILE%\AppData\Local\nvim
 copy nvim\init.lua %USERPROFILE%\AppData\Local\nvim\init.lua
-xcopy nvim\lua %USERPROFILE%\AppData\Local\nvim
-xcopy nvim\LuaSnip %USERPROFILE%\AppData\Local\nvim
+xcopy nvim\lua %USERPROFILE%\AppData\Local\nvim\lua
+xcopy nvim\LuaSnip %USERPROFILE%\AppData\Local\nvim\LuaSnip
 
 mkdir %APPDATA%\alacritty
 copy alacritty\alacritty.toml %APPDATA%\alacritty
